f7ce4a062b9b78a3d65b18d2d6451bc5cb8330da
seven456 <seven456@gmail.com>
2015-01-05 14:02:47 +0800

    添加：Apache License；

--- library/src/com/android/multiChannelPackageTool/MCPTool.java
+++ library/src/com/android/multiChannelPackageTool/MCPTool.java
@@ -1,5 +1,17 @@
 /*
- * 创建日期：2014年12月16日 下午18:56:29
+ * Copyright (C) 2014 seven456@gmail.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.android.multiChannelPackageTool;
 
@@ -25,9 +37,10 @@ import javax.crypto.spec.IvParameterSpec;
 
 /**
  * 多渠道打包工具；
+ * 创建时间： 2014-12-16 18:56:29
  * @author zhangguojun
  * @version 1.0
- * @since JDK1.6
+ * @since JDK1.6 Android2.2
  */
 public class MCPTool {
 	/**
