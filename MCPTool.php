e298ee30f7b587bb9e211bf3e1aaab5122faac83
belm <belm@users.noreply.github.com>
2016-03-23 16:23:38 +0800

    方便Android读取，末尾添加comment长度

--- MCPTool.php
+++ MCPTool.php
@@ -8,6 +8,7 @@
     if ($argc != 3) {
         echo 'example: php MCPTool.php xx.apk comment';echo PHP_EOL;exit;
     }
+
     $path    = trim($argv[1]);
     $comment = trim($argv[2]);
 
@@ -41,8 +42,9 @@
             $fp =  fopen($path , 'r+'); //这里注意不要使用rw
             //ZIP摘要结构 参考http://pingguohe.net/2016/03/21/Dynimac-write-infomation-into-apk.html
             $seek =  fseek($fp, -2, SEEK_END); //文件指针移到末尾2个字节 zip摘要长度信息占2个字节
-            $data = pack("v", strlen($comment)); //always 16 bit, little endian byte order
 
+            $len = strlen($comment);
+            $data = pack("v", $len + strlen($len)); //always 16 bit, little endian byte order
             $ret1 = fwrite($fp, $data);//返回写入的字符数
 
             if ($ret1 != 2) {
@@ -50,8 +52,9 @@
             }
 
             $ret2 = fwrite($fp, $comment);
+            $ret3 = fwrite($fp, pack("v", $len));
 
-            if ($ret2 === false) {
+            if ($ret2 === false || $ret3 === false) {
                 return 'write comment error';
             }
 
